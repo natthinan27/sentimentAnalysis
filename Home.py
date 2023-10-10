e2ef34f01979fe4c8177a91c0ce83b9d6a91eb51
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-10-10 16:04:38 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,8 +2,8 @@ import json
 import time
 import requests
 import streamlit as st
-#from streamlit_lottie import st_lottie
-#from streamlit_lottie import st_lottie_spinner
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie_spinner
 
 def load_lottieurl(url: str):
     r = requests.get(url)
@@ -19,9 +19,9 @@ col1, col2 = st.columns(2)
 with col1:    
     st.image('./pic/11.jpg')
 
-    #lot3="https://lottie.host/347e3388-72e3-4b11-9fe8-49b2349c4a7a/GekjLnvyXb.json"
-    #lottie3 = load_lottieurl(lot3)
-    #st_lottie(lottie3)
+    lot3="https://lottie.host/347e3388-72e3-4b11-9fe8-49b2349c4a7a/GekjLnvyXb.json"
+    lottie3 = load_lottieurl(lot3)
+    st_lottie(lottie3)
 with col2:
     st.image('./pic/DS1.jpg')
 st.balloons()
